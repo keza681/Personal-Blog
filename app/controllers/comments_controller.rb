ff498f8a43ac91f288353e542335b2b1a9f80a16
keza681 <lindakeza19@gmail.com>
2022-08-08 01:33:49 +0200

    Add comment and likes controllers to the project

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,27 @@
-class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User'
+class CommentsController < ApplicationController
+  def new
+    @add_new_comment = Comment.new
+  end
 
-  def update_comments_counter
-    post.increment!(:comments_counter)
+  def create
+    @post = Post.find(params[:post_id])
+    @add_new_comment = @post.comments.create(
+      text: comment_params[:text],
+      author_id: current_user.id,
+      post_id: @post.id
+    )
+    @add_new_comment.author_id = @post.id
+    @add_new_comment.post_id = params[:post_id]
+    @add_new_comment.update_comments_counter
+
+    if @add_new_comment.save
+      redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", notice: 'Successfully created your comment, congratulations!'
+    else
+      redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", alert: 'Oups!!! Something went wrong, faild to create your comment!'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,12 @@
-class Like < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User'
-
-  def update_likes_counter
-    post.increment!(:likes_counter)
+class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    updated_like = @post.likes.create(author_id: current_user.id, post_id: @post.id)
+    updated_like.update_counter_of_likes
+    if updated_like.save
+      redirect_to "/users/#{@post.author_id}/posts/#{@post.id}"
+    else
+      redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", alert: 'Oups! failed to add a like, do it later!!'
+    end
   end
 end
