7bff5d5286eba4d225b75481f81ccf7d5361ff64
keza681 <lindakeza19@gmail.com>
2022-08-08 01:21:26 +0200

    Implement the user view to the project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
-class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments, foreign_key: 'author_id', dependent: :destroy
-  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
-  def recent_posts
-    posts.limit(3).order(created_at: :desc)
+  def show
+    @user = User.find(params[:id])
   end
 end
