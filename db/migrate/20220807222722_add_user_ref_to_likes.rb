9b0b6670e5a2785c707887fd0d6742580e1d5de9
keza681 <lindakeza19@gmail.com>
2022-08-08 20:00:30 +0200

    Run migrations and generate schema

--- db/migrate/20220807222722_add_user_ref_to_likes.rb
+++ db/migrate/20220807222722_add_user_ref_to_likes.rb
@@ -1,8 +1,5 @@
-class CreateAddUserRefToLikes < ActiveRecord::Migration[7.0]
+class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
     add_reference :likes, :author, null: false, foreign_key: { to_table: :users }
-
-      t.timestamps
-    end
   end
 end
